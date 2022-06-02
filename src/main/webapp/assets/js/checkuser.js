988f21b431324c76aee64878777d0e78bfbfa6a8
viswanathanTJ <viswanathantj@gmail.com>
2022-06-02 20:44:20 +0530

    temp

--- src/main/webapp/assets/js/checkuser.js
+++ src/main/webapp/assets/js/checkuser.js
@@ -2,5 +2,5 @@ function logout() {
 	alert("logout");
     window.localStorage.removeItem("name");
     window.localStorage.removeItem("role");
-    window.location.href("login");
+    window.location.replace("login");
 }
