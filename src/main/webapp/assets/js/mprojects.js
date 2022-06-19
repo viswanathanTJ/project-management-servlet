7ab63808958faa41fa46a9be5423b5d68bc3a310
viswanathanTJ <viswanathantj@gmail.com>
2022-06-19 22:19:23 +0530

    Manager user added sidebar separated

--- src/main/webapp/assets/js/mprojects.js
+++ src/main/webapp/assets/js/mprojects.js
@@ -451,37 +451,3 @@ $(document).ready(function () {
 $("#cardId").on("click", function () {
   $("#popover__content").addClass("show");
 });
-
-// Sidebar toggle
-document.addEventListener("DOMContentLoaded", function (event) {
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
-
-    if (toggle && nav && bodypd && headerpd) {
-      toggle.addEventListener("click", () => {
-        nav.classList.toggle("show");
-        toggle.classList.toggle("bx-x");
-        bodypd.classList.toggle("body-pd");
-        headerpd.classList.toggle("body-pd");
-      });
-    }
-  };
-
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
-
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
-    }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
-
-  // Your code to run since DOM is loaded and ready
-});
